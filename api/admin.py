7ec1968e67ced588ec7a32ccefe4ae6c4429cc88
Roman Smirnov <54985705+Hyper-glitch@users.noreply.github.com>
2021-05-11 22:50:37 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -28,4 +28,4 @@ class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
-    # inlines = (UserProfileInline, )
+    inlines = (UserProfileInline, )
